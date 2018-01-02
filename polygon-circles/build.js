b9221bd93a302da5e1cb207e1a2a838f09c0ad17
Michael Bullington <mikebullingtn@gmail.com>
2018-01-02 17:48:04 -0500

    Clean up dev->build.js and simplify it, UglifyJS, add Mocha to makeshift tests.

--- polygon-circles/build.js
+++ polygon-circles/build.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+// just a wrapper for Browserify
 
 const browserify = require("browserify");
 // browserify transforms
@@ -6,7 +6,6 @@ const bubleify = require("bubleify");
 // browserify plugins
 const commonShake = require("common-shakeify");
 const packFlat = require("browser-pack-flat/plugin");
-const watchify = require("watchify");
 
 const instance = browserify("index.js", {
   cache: {},
@@ -24,24 +23,12 @@ instance.plugin(packFlat);
 let bundleCount = 0;
 function bundle() {
   bundleCount++;
-  console.log(`[BUNDLE] (${bundleCount} updates)`);
 
   instance.bundle()
     .on("error", err => {
       console.log(err.message);
     })
-    .pipe(fs.createWriteStream("./dist/index.js"));
-}
-
-if(process.argv.length >= 3 && process.argv[2] === "watch") {
-  const serve = require("serve");
-  serve(__dirname, {
-    port: 5000,
-    ignore: ["node_modules"]
-  });
-
-  instance.plugin(watchify);
-  instance.on("update", bundle);
+    .pipe(process.stdout);
 }
 
 bundle();
--- polygon-circles/test/index.js
+++ polygon-circles/test/index.js
@@ -1,23 +1,39 @@
-// TODO: Mocha?
+// do not run directly, run via npm/yarn or from Mocha
+
 const onInvoke = require("../")();
+
+function assertPromise(promise, test, message) {
+  if (test) {
+    promise.resolve();
+  } else {
+    promise.reject(new Error(message));
+  }
+}
 
 const testCases = ["one"];
 
-testCases.forEach(testCase => {
-  const input = require(`./${testCase}.input.js`);
-  const output = JSON.stringify(require(`./${testCase}.output.js`));
-
-  function getValue(key) {
-    if (input[key]) {
-      return input[key];
-    }
-  }
-  
-  function setValue(key, value) {
-    if (key === "geojson") {
-      console.log(`Test case ${testCase} ${value === output ? "succeeded": "failed"}`);
-    }
-  }
-
-  onInvoke({}, getValue, setValue);
+describe("Output tests", () => {
+  testCases.forEach(testCase => {
+    it(testCase, () => {
+      return new Promise((resolve, reject) => {
+        const input = require(`./${testCase}.input.js`);
+        const output = JSON.stringify(require(`./${testCase}.output.js`));
+      
+        function getValue(key) {
+          if (input[key]) {
+            return input[key];
+          }
+        }
+        
+        function setValue(key, value) {
+          if (key === "geojson") {
+            const message = `Output from test ${testCase} does not match expected output!`;
+            assertPromise({ resolve, reject }, value === output, message);
+          }
+        }
+      
+        onInvoke({}, getValue, setValue);
+      });
+    });
+  });
 });
